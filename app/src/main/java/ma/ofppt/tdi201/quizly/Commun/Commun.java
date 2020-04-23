15c3f9a6fd6a7c393bff1330030b1f8b6b4165f3
Mohammed-Aymen Benadra <ayman.benadra16@gmail.com>
2020-04-23 23:13:27 +0000

    added Game play code and start and game over layouts.

---
+++ app/src/main/java/ma/ofppt/tdi201/quizly/Commun/Commun.java
@@ -0,0 +1,13 @@
+package ma.ofppt.tdi201.quizly.Commun;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ma.ofppt.tdi201.quizly.Model.Question;
+import ma.ofppt.tdi201.quizly.Model.User;
+
+public class Commun {
+    public static String categoryId;
+    public static User currentUser;
+    public static List<Question> questionsList = new ArrayList<>();
+}
